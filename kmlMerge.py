a2417f9e61d8eba78e8ce314ff1fd570a316b26e
Kristof Coninx <kristof.coninx@cs.kuleuven.be>
2014-09-25 14:39:52 +0200

    Fixed some code style issues.

--- kmlMerge.py
+++ kmlMerge.py
@@ -1,23 +1,27 @@
 __author__ = 'kristofc'
-from xml.etree import ElementTree as et
+from xml.etree import ElementTree as eT
 import sys
+
+OUTPUT_FILENAME = "MergedOutput.kml"
+NAMESPACE = "http://earth.google.com/kml/2.0"
+
 
 class XMLCombiner(object):
     def __init__(self, filenames):
         assert len(filenames) > 0, 'No filenames!'
-        et.register_namespace('', "http://earth.google.com/kml/2.0")
+        eT.register_namespace('', NAMESPACE)
         # save all the roots, in order, to be processed later
-        self.trees = [et.parse(f) for f in filenames]
+        self.trees = [eT.parse(f) for f in filenames]
         self.roots = [t.getroot() for t in self.trees]
 
     def combine(self):
-        rootCoord = self.roots[0].findall(".//{http://earth.google.com/kml/2.0}coordinates")
+        rootcoordinate = self.roots[0].findall(".//{http://earth.google.com/kml/2.0}coordinates")
         for r in self.trees[1:]:
-            otherCoord = r.findall(".//{http://earth.google.com/kml/2.0}coordinates")
-            newc = str(rootCoord[0].text) + str(otherCoord[0].text)
-            filtered = "\n".join([ll.rstrip() for ll in newc.splitlines() if ll.strip()])
-            rootCoord[0].text = str(filtered)
-            self.trees[0].write("MergedOutput.kml")
+            othercoordinate = r.findall(".//{http://earth.google.com/kml/2.0}coordinates")
+            newcoordinate = str(rootcoordinate[0].text) + str(othercoordinate[0].text)
+            filtered = "\n".join([ll.rstrip() for ll in newcoordinate.splitlines() if ll.strip()])
+            rootcoordinate[0].text = str(filtered)
+            self.trees[0].write(OUTPUT_FILENAME)
 
 
 if __name__ == '__main__':
@@ -25,4 +29,3 @@ if __name__ == '__main__':
         XMLCombiner(sys.argv[1:]).combine()
     else:
         print("Supply the files you want to merge on the commandline in the right order.")
-
